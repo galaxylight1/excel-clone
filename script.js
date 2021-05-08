50e0784f7f10427db2bdb36aaa7a7a8c0722e5ce
galaxylight1 <prateeksingh8841@gmail.com>
2021-05-08 10:52:10 +0530

    added_cells_and_scrollbar

---
+++ script.js
@@ -0,0 +1,43 @@
+
+const ps = new PerfectScrollbar('#cells', {
+    wheelSpeed: 2
+});
+
+for(let i = 1; i <= 100; i++)
+{
+    let str = '';
+    let n = i;
+
+    while(n > 0)
+    {
+        let rem = n % 26;
+        if(rem === 0)
+        {
+            str = 'Z' + str;
+            n = Math.floor(n / 26) - 1;
+        }
+        else
+        {
+            str = String.fromCharCode((rem-1) + 65) + str;
+            n = Math.floor(n / 26);
+        }
+    }
+
+    $('#columns').append(`<div class="column-name">${str}</div>`);
+    $('#rows').append(`<div class="row-name">${i}</div>`);
+}
+
+for(let i = 1; i <= 100; i++)
+{
+    let row = $('<div class="cell-row"></div>');
+    for(let j = 1; j <= 100; j++)
+    {
+        row.append(`<div id="row-${i}-col-${j}" class="input-cell" contenteditable="false"></div>`);
+    }
+    $('#cells').append(row);
+}
+
+$('#cells').scroll(function(e) {
+    $('#columns').scrollLeft(this.scrollLeft); // only scrollLeft exists in perfectScrollbar
+    $('#rows').scrollTop(this.scrollTop); // only scrollLeft exists in perfectScrollbar
+});
