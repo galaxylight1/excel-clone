e815d09057d1f03c1fcd89b5843dba825a3234f9
galaxylight1 <prateeksingh8841@gmail.com>
2021-05-08 13:24:20 +0530

    added_single_select

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ for(let i = 1; i <= 100; i++)
 
 for(let i = 1; i <= 100; i++)
 {
-    let row = $('<div class="cell-row"></div>');
+    let row = $('<div class="cell-row"></div>'); // createElement using jQuery
     for(let j = 1; j <= 100; j++)
     {
         row.append(`<div id="row-${i}-col-${j}" class="input-cell" contenteditable="false"></div>`);
@@ -37,7 +37,26 @@ for(let i = 1; i <= 100; i++)
     $('#cells').append(row);
 }
 
+// fixing scroll of #columns and #rows when scrolling with perfectScrollbar
+
 $('#cells').scroll(function(e) {
     $('#columns').scrollLeft(this.scrollLeft); // only scrollLeft exists in perfectScrollbar
     $('#rows').scrollTop(this.scrollTop); // only scrollLeft exists in perfectScrollbar
 });
+
+// select and unselect cells
+
+function getRowCol(ele) {
+    let arr = $(ele).attr('id').split('-');
+    return [parseInt(arr[1]), parseInt(arr[3])];
+}
+
+$('.input-cell').click(function(e) {
+    let [rowId, colId] = getRowCol(this);
+    selectCell(this);
+});
+
+function selectCell(ele) {
+    $('.input-cell.selected').removeClass('selected');
+    $(ele).addClass('selected');
+} 
